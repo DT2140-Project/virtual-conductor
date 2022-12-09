8b341abe02b5798f4ef61fee58fde73fd3feee06
Erik Henriksson <erik.ch.henriksson@gmail.com>
2022-12-09 10:30:35 +0100

    Initial playback logic for conductor scenario, 2 instruments and 2 bars, switchable intensity

--- my.js
+++ my.js
@@ -1,2 +1,8 @@
-window.onload = function() {
+window.onload = function () {};
+
+function setGuitarIntensity(value) {
+  iMusic.select("intensity_guitar", value);
+}
+function setStringsIntensity(value) {
+  iMusic.select("intensity_strings", value);
 }
